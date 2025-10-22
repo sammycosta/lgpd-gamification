84757e977bc94ad7f206b57b95381e00f9c9af08
sammycosta <arminetah@hotmail.com>
2025-10-22 00:27:40 -0300

    base definicao de conteudos

---
+++ app/apps/web/src/app/resources/[id]/IntroducaoLGPDContent.tsx
@@ -0,0 +1,12 @@
+import { Title } from '@mantine/core'
+
+export default function IntroducaoLGPDContent() {
+  return (
+    <>
+      <Title order={1} mb="sm">
+        Introdução à LGPD
+      </Title>
+      ...CONTEÚDO!
+    </>
+  )
+}
--- app/apps/web/src/app/resources/[id]/page.tsx
+++ app/apps/web/src/app/resources/[id]/page.tsx
@@ -1,7 +1,7 @@
-import { MODULE_DATA } from '@/app/mockData'
 import ButtonGoBack from '@/components/ui/ButtonGoBack'
-import { Badge, Group, Title } from '@mantine/core'
+import { Badge, Group } from '@mantine/core'
 import { NotebookText } from 'lucide-react'
+import IntroducaoLGPDContent from './IntroducaoLGPDContent'
 
 interface ResourcesPageProps {
   params: Promise<{ id: string }>
@@ -11,28 +11,25 @@ export default async function ResourcesPage({ params }: ResourcesPageProps) {
   const { id } = await params
 
   const moduleId = Number(id)
-  const module = MODULE_DATA.find((m) => m.id === moduleId)
-
-  if (!module) {
-    return <div>Módulo não encontrado</div>
-  }
 
   return (
     <>
       <ButtonGoBack />
       <Group mt="md">
-        <Badge
-          variant="light"
-          size="lg"
-          leftSection={<NotebookText size={16} />}
-          radius="sm"
-        >
+        <Badge variant="light" size="lg" leftSection={<NotebookText size={16} />} radius="sm">
           Materiais de Apoio
         </Badge>
       </Group>
-      <Title order={1} mb="sm">
-        {module.name}
-      </Title>
+      <Content moduleId={moduleId} />
     </>
   )
+}
+
+const Content = ({ moduleId }: { moduleId: number }) => {
+  switch (moduleId) {
+    case 1:
+      return <IntroducaoLGPDContent />
+    default:
+      return <div>Conteúdo não encontrado!</div>
+  }
 }
